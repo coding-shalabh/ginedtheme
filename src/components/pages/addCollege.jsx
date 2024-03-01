5cb392f67ca74417b8e28f8f6ddcfd482c12e2d3
coding-shalabh <marketingshalabh.com>
2024-03-02 02:38:28 +0530

    Post college is fixed

--- src/components/pages/addCollege.jsx
+++ src/components/pages/addCollege.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import CourseHeader from "./course/header";
 
 const AddCollege = () => {
+
   const [formData, setFormData] = useState({
     name: "",
     about: "",
+    excerpt: "",
     placements: {
-      companies: [],
+      companies: "",
       highestPackage: "",
       averagePackage: "",
       percentagePlaced: ""
     },
     virtualTourLink: "",
-    youtubeVideos: [],
+    youtubeVideos: ["", "", "", ""], // Initialize with 4 empty strings for YouTube videos
     reviews: "",
-    courses: []
+    courses: [] // Use an array to hold multiple course IDs
   });
+
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
@@ -35,98 +36,153 @@ const AddCollege = () => {
   }, []);
 
   const handleInputChange = (e) => {
-    if (["highestPackage", "averagePackage", "percentagePlaced"].includes(e.target.name)) {
+    if (e.target.name === "courses") {
+      // Handle multiple selections for courses
+      const selectedOptions = Array.from(e.target.selectedOptions).map(option => option.value);
+      setFormData(prevFormData => ({
+        ...prevFormData,
+        courses: selectedOptions
+      }));
+    } else if (e.target.name.startsWith("youtubeVideos")) {
+      // Handle YouTube video inputs
+      const index = parseInt(e.target.name.replace("youtubeVideos", ""), 10);
+      const newYoutubeVideos = [...formData.youtubeVideos];
+      newYoutubeVideos[index] = e.target.value;
+      setFormData(prevFormData => ({
+        ...prevFormData,
+        youtubeVideos: newYoutubeVideos
+      }));
+    }else if (["highestPackage", "averagePackage", "percentagePlaced", "companies"].includes(e.target.name)) {
       setFormData({
         ...formData,
         placements: {
           ...formData.placements,
           [e.target.name]: e.target.value
         }
-      });
-    } else {
-      setFormData({ ...formData, [e.target.name]: e.target.value });
+      })
+    } 
+    else {
+      // Handle other inputs including placements
+      setFormData(prevFormData => ({
+        ...prevFormData,
+        [e.target.name]: e.target.value
+      }));
     }
   };
 
-  const handleEditorChange = (name, editorData) => {
-    setFormData({ ...formData, [name]: editorData });
-  };
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
+
+try {
+    const response = await axios.post('https://api.gined.in/api/colleges', formData);
+    console.log('Successfully submitted:', response.data);
     console.log(formData);
-    try {
-      await axios.post("https://api.gined.in/api/colleges", formData, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      alert("College successfully saved!");
-      // Optionally, reset the form state here
-    } catch (error) {
-      console.error("Error saving college:", error);
-      alert("Failed to save college.");
-    }
-  };
+    alert('Form submitted successfully!');
+    // Reset the form data after successful submission
+    setFormData({
+      name: "",
+      about: "",
+      excerpt: "",
+      placements: {
+        companies: "",
+        highestPackage: "",
+        averagePackage: "",
+        percentagePlaced: ""
+      },
+      virtualTourLink: "",
+      youtubeVideos: ["", "", "", ""],
+      reviews: "",
+      courses: []
+    });
+  } catch (error) {
+    console.error('Error submitting form:', error.response ? error.response.data : error);
+    alert('Failed to submit the form.');
+  }
+    };
 
   return (
     <>
       <div>
         <CourseHeader />
         <fieldset style={{ marginTop: "20px", width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }} id="first">
-        <form onSubmit={handleSubmit}>
-          {/* Form fields here */}
-          <div className="form-group">
-            <label>College Name</label>
-            <input type="text" name="collegeName" className="form-control" value={formData.collegeName} onChange={handleInputChange} />
-          </div>
-          <div className="form-group">
-            <label>About the College</label>
-            <CKEditor editor={ClassicEditor} data={formData.aboutCollege} onChange={(event, editor) => handleEditorChange(event, editor, "aboutCollege")} />
-          </div>
-          <div className="form-group">
-                  <label className="add-course-label">Short Description</label>
-                  <textarea className="form-control" placeholder="Short Description for College"></textarea>
-                </div>
-                <div className="form-group">
-                  <label className="add-course-label">Virtual Tour Link</label>
-                  <input type="text" className="form-control" placeholder="Virtual Tour Link" />
-                </div>
-                <div className="form-group">
-                  <label className="add-course-label">Review for College</label>
-                  <input type="number" className="form-control" placeholder="Review for College" />
-                </div>
-                <div className="form-group">
-                  <label className="add-course-label">Placement Companies</label>
-                  <input type="text" className="form-control" placeholder="Placement Companies" />
-                </div>
-                <div className="form-group">
-                  <label className="add-course-label">Highest Package</label>
-                  <input type="text" className="form-control" placeholder="Highest Package" />
-                </div>
-                <div className="form-group">
-                  <label className="add-course-label">Average Package</label>
-                  <input type="text" className="form-control" placeholder="Average Package" />
-                </div>
-                <div className="form-group">
-                  <label className="add-course-label">Percentage Placed</label>
-                  <input type="number" className="form-control" placeholder="Percentage Placed" />
-                </div>
-          <div className="form-group">
-            <label>Course Selection</label>
-            <select className="form-control" name="selectedCourse" value={formData.selectedCourse} onChange={handleInputChange}>
-              <option value="">Select a course</option>
-              {courses.map(course => <option key={course._id} value={course._id}>{course.title}</option>)}
-            </select>
-          </div>
-          <div className="form-group">
-            <label>Upload College Image (1920px x 200px)</label>
-            <input type="file" className="form-control" />
-            <small>Please upload an image with size 1920px x 200px.</small>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+          <form onSubmit={handleSubmit}>
+            {/* Form fields here */}
+            <div className="form-group">
+              <label>College Name</label>
+              <input type="text" name="name" className="form-control" value={formData.name} onChange={handleInputChange} />
+            </div>
+            <div className="form-group">
+              <label>About the College</label>
+              <textarea className="form-control" name="about" placeholder="About College" value={formData.about} onChange={handleInputChange} ></textarea>
+            </div>
+            <div className="form-group">
+              <label className="add-course-label">Short Description</label>
+              <textarea className="form-control" placeholder="Short Description for College" name="excerpt" value={formData.excerpt} onChange={handleInputChange} ></textarea>
+            </div>
+            <div className="form-group">
+              <label className="add-course-label">Virtual Tour Link</label>
+              <input type="text" className="form-control" placeholder="Virtual Tour Link" name="virtualTourLink" value={formData.virtualTourLink} onChange={handleInputChange} />
+            </div>
+            <div className="form-group">
+              <label className="add-course-label">Review for College</label>
+              <input type="number" className="form-control" placeholder="Review for College" name="reviews" value={formData.reviews} onChange={handleInputChange} />
+            </div>
+            <div className="form-group">
+              <label className="add-course-label">Placement Companies</label>
+              <input type="text" className="form-control" placeholder="Placement Companies" name="companies" value={formData.companies} onChange={handleInputChange} />
+            </div>
+            <div className="form-group">
+              <label className="add-course-label">Highest Package</label>
+              <input type="text" className="form-control" placeholder="Highest Package" name="highestPackage" value={formData.highestPackage} onChange={handleInputChange} />
+            </div>
+            <div className="form-group">
+              <label className="add-course-label">Average Package</label>
+              <input type="text" className="form-control" placeholder="Average Package" name="averagePackage" value={formData.averagePackage} onChange={handleInputChange} />
+            </div>
+            <div className="form-group">
+              <label className="add-course-label">Percentage Placed</label>
+              <input type="number" className="form-control" placeholder="Percentage Placed" name="percentagePlaced" value={formData.percentagePlaced} onChange={handleInputChange} />
+            </div>
+            {formData.youtubeVideos.map((video, index) => (
+              <div className="form-group" key={index}>
+                <label>YouTube Video {index + 1}</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name={`youtubeVideos${index}`}
+                  value={video}
+                  onChange={handleInputChange}
+                  placeholder={`YouTube Video ${index + 1} URL`}
+                />
+              </div>
+            ))}
+
+            <div className="form-group">
+              <label>Course Selection</label>
+              <select
+                className="form-control"
+                name="courses"
+                value={formData.courses}
+                onChange={handleInputChange}
+                multiple={true} // Allow multiple selections
+              >
+                <option value="" disabled>Select courses</option>
+                {courses.map(course => (
+                  <option key={course._id} value={course._id}>{course.title}</option>
+                ))}
+              </select>
+              <small>Hold down the Ctrl (windows) / Command (Mac) button to select multiple options.</small>
+            </div>
+            <div className="form-group">
+              <label>Upload College Image (1920px x 200px)</label>
+              <input type="file" className="form-control" />
+              <small>Please upload an image with size 1920px x 200px.</small>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
         </fieldset>
       </div>
     </>
